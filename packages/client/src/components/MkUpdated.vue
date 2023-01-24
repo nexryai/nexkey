7e13f3c8c8e33aaa6b2ffa34a410ef05d00067ec
atsu1125 <atsuchan@atsuchan.page>
2023-01-24 14:02:43 +0900

    Fix: Update Notification

--- packages/client/src/components/MkUpdated.vue
+++ packages/client/src/components/MkUpdated.vue
@@ -21,7 +21,7 @@ const modal = ref<InstanceType<typeof MkModal>>();
 
 const whatIsNew = () => {
 	modal.value.close();
-	window.open(`https://misskey-hub.net/docs/releases.html#_${version.replace(/\./g, '-')}`, '_blank');
+	window.open(`https://github.com/atsu1125/misskey-v12/releases/tag/${version}`, '_blank');
 };
 </script>
 
