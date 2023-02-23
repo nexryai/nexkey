438658c96f18889426b34414c7f3d03e17fad207
atsu1125 <atsuchan@atsuchan.page>
2023-02-23 19:21:57 +0900

    Add Widgets profile and instanceInfo

--- packages/client/src/widgets/index.ts
+++ packages/client/src/widgets/index.ts
@@ -1,6 +1,8 @@
 import { App, defineAsyncComponent } from 'vue';
 
 export default function(app: App) {
+	app.component('MkwProfile', defineAsyncComponent(() => import('./profile.vue')));
+	app.component('MkwInstanceInfo', defineAsyncComponent(() => import('./instance-info.vue')));
 	app.component('MkwMemo', defineAsyncComponent(() => import('./memo.vue')));
 	app.component('MkwNotifications', defineAsyncComponent(() => import('./notifications.vue')));
 	app.component('MkwTimeline', defineAsyncComponent(() => import('./timeline.vue')));
@@ -23,9 +25,12 @@ export default function(app: App) {
 	app.component('MkwButton', defineAsyncComponent(() => import('./button.vue')));
 	app.component('MkwAiscript', defineAsyncComponent(() => import('./aiscript.vue')));
 	app.component('MkwAichan', defineAsyncComponent(() => import('./aichan.vue')));
+
 }
 
 export const widgets = [
+	'profile',
+	'instanceInfo',
 	'memo',
 	'notifications',
 	'timeline',
