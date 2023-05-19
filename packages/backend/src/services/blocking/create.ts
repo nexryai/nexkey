f2893f8d054cb2b1c70aa8a3ccf04c5cdd7cc0a7
atsu1125 <atsuchan@atsuchan.page>
2023-05-19 11:49:33 +0900

    BlockでRejectFollowを送信

--- packages/backend/src/services/blocking/create.ts
+++ packages/backend/src/services/blocking/create.ts
@@ -129,6 +129,12 @@ async function unFollow(follower: User, followee: User) {
 		const content = renderActivity(renderUndo(renderFollow(follower, followee), follower));
 		deliver(follower, content, followee.inbox);
 	}
+
+	// リモートにフォローをされていたらRejectFollow送信
+	if (Users.isLocalUser(followee) && Users.isRemoteUser(follower)) {
+		const content = renderActivity(renderReject(renderFollow(follower, followee), follower));
+		deliver(followee, content, follower.inbox);
+	}
 }
 
 async function removeFromList(listOwner: User, user: User) {
