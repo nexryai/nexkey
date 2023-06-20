e630af20c6599afdea44a5eb36899a0f42881fa8
atsu1125 <atsuchan@atsuchan.page>
2023-06-20 13:43:13 +0900

    FollowRequestsもdelete

--- packages/backend/src/queue/processors/db/delete-account.ts
+++ packages/backend/src/queue/processors/db/delete-account.ts
@@ -1,6 +1,6 @@
 import Bull from 'bull';
 import { queueLogger } from '../../logger.js';
-import { AccessTokens, DriveFiles, Notes, UserProfiles, Users, UserNotePinings, MessagingMessages, Followings, Mutings, Blockings, Notifications } from '@/models/index.js';
+import { AccessTokens, DriveFiles, Notes, UserProfiles, Users, UserNotePinings, MessagingMessages, Followings, Mutings, Blockings, Notifications, FollowRequests } from '@/models/index.js';
 import { DbUserDeleteJobData } from '@/queue/types.js';
 import { Note } from '@/models/entities/note.js';
 import { DriveFile } from '@/models/entities/drive-file.js';
@@ -149,6 +149,12 @@ export async function deleteAccount(job: Bull.Job<DbUserDeleteJobData>): Promise
 			});
 			await Notifications.delete({
 				notifieeId: job.data.user.id,
+			});
+			await FollowRequests.delete({
+				followerId: job.data.user.id,
+			});
+			await FollowRequests.delete({
+				followeeId: job.data.user.id,
 			});
 		} else {
 			await Users.delete(job.data.user.id);
