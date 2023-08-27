b073b26d93fec6044afd195038ff5006e716cc8b
nexryai <git@nexryai.online>
2023-08-27 11:51:07 +0900

    remove debug code

--- packages/client/src/scripts/check-reaction-mute.ts
+++ packages/client/src/scripts/check-reaction-mute.ts
@@ -1,6 +1,4 @@
 export function checkReactionMute(reaction: string, mutedReactions: Array<string | string[]>) {
-	console.log(reaction)
-	console.log(mutedReactions)
 	return mutedReactions.some(item => {
 		if (Array.isArray(item)) {
 			return item.includes(reaction);
