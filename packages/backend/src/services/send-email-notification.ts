2f95f1802d221850058bcd6677cdc4f77cdf33f7
atsu1125 <atsuchan@atsuchan.page>
2023-09-30 10:57:36 +0900

    Enhance Email Notification

--- packages/backend/src/services/send-email-notification.ts
+++ packages/backend/src/services/send-email-notification.ts
@@ -19,7 +19,42 @@ async function receiveFollowRequest(userId: User['id'], follower: User) {
 	sendEmail(userProfile.email, `New Follow Request`, `${follower.name} (@${Acct.toString(follower)})`, `${follower.name} (@${Acct.toString(follower)})`);
 }
 
+async function reply(userId: User['id'], follower: User, customBody: string) {
+	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	if (!userProfile.email || !userProfile.emailNotificationTypes.includes('reply')) return;
+	sendEmail(userProfile.email, `New Reply`, `${follower.name} (@${Acct.toString(follower)}) <br> ${customBody}`, `${follower.name} (@${Acct.toString(follower)}) ${customBody}`);
+}
+
+async function mention(userId: User['id'], follower: User, customBody: string) {
+	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	if (!userProfile.email || !userProfile.emailNotificationTypes.includes('mention')) return;
+	sendEmail(userProfile.email, `New Mention`, `${follower.name} (@${Acct.toString(follower)}) <br> ${customBody}`, `${follower.name} (@${Acct.toString(follower)}) ${customBody}`);
+}
+
+async function quote(userId: User['id'], follower: User, customBody: string, url: string) {
+	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	if (!userProfile.email || !userProfile.emailNotificationTypes.includes('quote')) return;
+	sendEmail(userProfile.email, `New Quote`, `${follower.name} (@${Acct.toString(follower)}) <br> ${customBody} <br> ${url}`, `${follower.name} (@${Acct.toString(follower)}) ${customBody} ${url}`);
+}
+
+async function groupInvited(userId: User['id'], customBody: string) {
+	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	if (!userProfile.email || !userProfile.emailNotificationTypes.includes('groupInvited')) return;
+	sendEmail(userProfile.email, `New Group Invitation`, `${customBody}`, `${customBody}`);
+}
+
+async function app(userId: User['id'], customHeader: string, customBody: string) {
+	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	if (!userProfile.email || !userProfile.emailNotificationTypes.includes('app')) return;
+	sendEmail(userProfile.email, `New Application Notice`, `${customHeader} <br> ${customBody}`, `${customHeader} ${customBody}`);
+}
+
 export const sendEmailNotification = {
 	follow,
 	receiveFollowRequest,
+	reply,
+	mention,
+	quote,
+	groupInvited,
+	app,
 };
