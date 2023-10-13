a0a6e4ee0a992ffe56dfd8b843ba746ecfff046d
nexryai <nexryai@sda1.net>
2023-10-13 11:31:48 +0900

    fix

--- packages/backend/src/server/api/index.ts
+++ packages/backend/src/server/api/index.ts
@@ -15,9 +15,6 @@ import handler from './api-handler.js';
 import signup from './private/signup.js';
 import signin from './private/signin.js';
 import signupPending from './private/signup-pending.js';
-import discord from './service/discord.js';
-import github from './service/github.js';
-import twitter from './service/twitter.js';
 
 // Init app
 const app = new Koa();
@@ -81,9 +78,6 @@ router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/signup-pending', signupPending);
 
-router.use(discord.routes());
-router.use(github.routes());
-router.use(twitter.routes());
 
 router.get('/v1/instance/peers', async ctx => {
 	const instances = await Instances.find({
