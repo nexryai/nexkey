99309f4a16e1f53314e27695bddd89a1c01b1498
atsu1125 <atsuchan@atsuchan.page>
2023-10-22 19:05:51 +0900

    Do not send Email notification to Suspended User

--- packages/backend/src/services/send-email-notification.ts
+++ packages/backend/src/services/send-email-notification.ts
@@ -1,4 +1,4 @@
-import { UserProfiles } from '@/models/index.js';
+import { UserProfiles, Users } from '@/models/index.js';
 import { User } from '@/models/entities/user.js';
 import { sendEmail } from './send-email.js';
 import * as Acct from '@/misc/acct.js';
@@ -10,43 +10,57 @@ import { emailDeliver } from '@/queue/index.js';
 
 async function follow(userId: User['id'], follower: User) {
 	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	const userDetailed = await Users.findOneByOrFail({ id: userId });
 	if (!userProfile.email || !userProfile.emailVerified || !userProfile.emailNotificationTypes.includes('follow')) return;
+	if (userDetailed.isSuspended) throw new Error ('User is not active.');
 	emailDeliver(userProfile.email, `New Follower`, `${follower.name} (@${Acct.toString(follower)})`, `${follower.name} (@${Acct.toString(follower)})`);
 }
 
 async function receiveFollowRequest(userId: User['id'], follower: User) {
 	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	const userDetailed = await Users.findOneByOrFail({ id: userId });
 	if (!userProfile.email || !userProfile.emailVerified || !userProfile.emailNotificationTypes.includes('receiveFollowRequest')) return;
+	if (userDetailed.isSuspended) throw new Error ('User is not active.');
 	emailDeliver(userProfile.email, `New Follow Request`, `${follower.name} (@${Acct.toString(follower)})`, `${follower.name} (@${Acct.toString(follower)})`);
 }
 
 async function reply(userId: User['id'], follower: User, customBody: string) {
 	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	const userDetailed = await Users.findOneByOrFail({ id: userId });
 	if (!userProfile.email || !userProfile.emailVerified || !userProfile.emailNotificationTypes.includes('reply')) return;
+	if (userDetailed.isSuspended) throw new Error ('User is not active.');
 	emailDeliver(userProfile.email, `New Reply`, `${follower.name} (@${Acct.toString(follower)}) <br> ${customBody}`, `${follower.name} (@${Acct.toString(follower)}) ${customBody}`);
 }
 
 async function mention(userId: User['id'], follower: User, customBody: string) {
 	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	const userDetailed = await Users.findOneByOrFail({ id: userId });
 	if (!userProfile.email || !userProfile.emailVerified || !userProfile.emailNotificationTypes.includes('mention')) return;
+	if (userDetailed.isSuspended) throw new Error ('User is not active.');
 	emailDeliver(userProfile.email, `New Mention`, `${follower.name} (@${Acct.toString(follower)}) <br> ${customBody}`, `${follower.name} (@${Acct.toString(follower)}) ${customBody}`);
 }
 
 async function quote(userId: User['id'], follower: User, customBody: string, url: string) {
 	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	const userDetailed = await Users.findOneByOrFail({ id: userId });
 	if (!userProfile.email || !userProfile.emailVerified || !userProfile.emailNotificationTypes.includes('quote')) return;
+	if (userDetailed.isSuspended) throw new Error ('User is not active.');
 	emailDeliver(userProfile.email, `New Quote`, `${follower.name} (@${Acct.toString(follower)}) <br> ${customBody} <br> ${url}`, `${follower.name} (@${Acct.toString(follower)}) ${customBody} ${url}`);
 }
 
 async function groupInvited(userId: User['id'], customBody: string) {
 	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	const userDetailed = await Users.findOneByOrFail({ id: userId });
 	if (!userProfile.email || !userProfile.emailVerified || !userProfile.emailNotificationTypes.includes('groupInvited')) return;
+	if (userDetailed.isSuspended) throw new Error ('User is not active.');
 	emailDeliver(userProfile.email, `New Group Invitation`, `${customBody}`, `${customBody}`);
 }
 
 async function app(userId: User['id'], customHeader: string, customBody: string) {
 	const userProfile = await UserProfiles.findOneByOrFail({ userId: userId });
+	const userDetailed = await Users.findOneByOrFail({ id: userId });
 	if (!userProfile.email || !userProfile.emailVerified || !userProfile.emailNotificationTypes.includes('app')) return;
+	if (userDetailed.isSuspended) throw new Error ('User is not active.');
 	emailDeliver(userProfile.email, `New Application Notice`, `${customHeader} <br> ${customBody}`, `${customHeader} ${customBody}`);
 }
 
