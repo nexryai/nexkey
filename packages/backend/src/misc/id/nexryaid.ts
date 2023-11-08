a75c483eea90ce3a5b0d705557dfb1b213e1762e
scil-t <116360839+scil-t@users.noreply.github.com>
2023-11-08 20:48:39 +0900

    nexryaidを追加

---
+++ packages/backend/src/misc/id/nexryaid.ts
@@ -0,0 +1,27 @@
+// nexryaid
+// nexryaiからランダムで並べ替え(重複あり)+unixtime+適当にゃ数字で作るid 
+// ex) neryax1699364743467
+import * as crypto from 'node:crypto';
+
+const nexryaiArray = ["n","e","x","r","y","a","i"];
+
+function getRandomNexryai() {
+    return nexryaiArray.sort(() => Math.random() - 0.5).join("");
+}
+
+function getRandInt() {
+    const buff = crypto.randomBytes(1);
+    return parseInt(buff.toString('hex'), 16);
+}
+
+export function genNexryaid(date: Date): string {
+
+    const nexryai = getRandomNexryai();
+    const unixtime = Math.floor(date.getTime() / 1000 );
+    const randInt = getRandInt();
+
+    if (isNaN(unixtime)) throw 'Failed to create nexryaid: Invalid Date';
+
+    return `${nexryai}${unixtime}${randInt}`;
+}
+
