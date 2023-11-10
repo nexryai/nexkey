3a363e078b53751009262a6a0d642ce944e61314
atsu1125 <atsuchan@atsuchan.page>
2023-11-10 11:02:39 +0900

    fix(backend): isBot not being set on Application type

--- packages/backend/src/remote/activitypub/models/person.ts
+++ packages/backend/src/remote/activitypub/models/person.ts
@@ -155,7 +155,7 @@ export async function createPerson(uri: string, resolver?: Resolver): Promise<Us
 
 	const tags = extractApHashtags(person.tag).map(tag => normalizeForSearch(tag)).splice(0, 32);
 
-	const isBot = getApType(object) === 'Service';
+	const isBot = getApType(object) === 'Service' || getApType(object) === 'Application';
 
 	const bday = person['vcard:bday']?.match(/^\d{4}-\d{2}-\d{2}/);
 
@@ -340,7 +340,7 @@ export async function updatePerson(uri: string, resolver?: Resolver | null, hint
 		emojis: emojiNames,
 		name: truncate(person.name, nameLength),
 		tags,
-		isBot: getApType(object) === 'Service',
+		isBot: getApType(object) === 'Service' || getApType(object) === 'Application',
 		isCat: (person as any).isCat === true,
 		isLocked: !!person.manuallyApprovesFollowers,
 		isExplorable: !!person.discoverable,
