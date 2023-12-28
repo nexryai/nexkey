aa9e68a7ba76e6b97c3707f32172e2f58ab8cd7f
nexryai <nexryai@sda1.net>
2023-12-28 15:40:11 +0900

    Client: セキュリティキーのサポートを一旦廃止

--- packages/client/src/pages/settings/2fa.vue
+++ packages/client/src/pages/settings/2fa.vue
@@ -14,41 +14,6 @@
         </MkInfo>
         <p>{{ i18n.ts._2fa.alreadyRegistered }}</p>
         <MkButton @click="unregister">{{ i18n.ts.unregister }}</MkButton>
-
-        <template v-if="supportsCredentials">
-            <hr class="totp-method-sep">
-
-            <h4>{{ i18n.ts.securityKey }}</h4>
-            <p>{{ i18n.ts._2fa.securityKeyInfo }}</p>
-            <div class="key-list">
-                <div v-for="key in $i.securityKeysList" class="key">
-                    <h3>{{ key.name }}</h3>
-                    <div class="last-used">{{ i18n.ts.lastUsed }}<MkTime :time="key.lastUsed"/></div>
-                    <MkButton @click="unregisterKey(key)">{{ i18n.ts.unregister }}</MkButton>
-                </div>
-            </div>
-
-            <MkSwitch v-if="$i.securityKeysList.length > 0" v-model="usePasswordLessLogin" @update:modelValue="updatePasswordLessLogin">{{ i18n.ts.passwordLessLogin }}</MkSwitch>
-
-            <MkInfo v-if="registration && registration.error" warn>{{ i18n.ts.error }} {{ registration.error }}</MkInfo>
-            <MkButton v-if="!registration || registration.error" @click="addSecurityKey">{{ i18n.ts._2fa.registerKey }}</MkButton>
-
-            <ol v-if="registration && !registration.error">
-                <li v-if="registration.stage >= 0">
-                    {{ i18n.ts.tapSecurityKey }}
-                    <MkLoading v-if="registration.saving && registration.stage == 0" :em="true"/>
-                </li>
-                <li v-if="registration.stage >= 1">
-                    <MkForm :disabled="registration.stage != 1 || registration.saving">
-                        <MkInput v-model="keyName" :max="30">
-                            <template #label>{{ i18n.ts.securityKeyName }}</template>
-                        </MkInput>
-                        <MkButton :disabled="keyName.length == 0" @click="registerKey">{{ i18n.ts.registerSecurityKey }}</MkButton>
-                        <MkLoading v-if="registration.saving && registration.stage == 1" :em="true"/>
-                    </MkForm>
-                </li>
-            </ol>
-        </template>
     </template>
     <div v-if="twoFactorData && !$i.twoFactorEnabled">
         <ol style="margin: 0; padding: 0 0 0 1em;">
