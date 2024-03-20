95cb7cf51e57888b31f608e28a5bc4d8a0d1cec7
atsu1125 <atsuchan@atsuchan.page>
2024-03-20 10:03:17 +0900

    Fix: Delete remote followings when local user is Suspended

--- packages/backend/src/server/api/endpoints/admin/suspend-user.ts
+++ packages/backend/src/server/api/endpoints/admin/suspend-user.ts
@@ -5,6 +5,7 @@ import { User } from '@/models/entities/user.js';
 import { insertModerationLog } from '@/services/insert-moderation-log.js';
 import { doPostSuspend } from '@/services/suspend-user.js';
 import { publishUserEvent } from '@/services/stream.js';
+import { Not, IsNull } from 'typeorm';
 
 export const meta = {
 	tags: ['admin'],
@@ -51,6 +52,8 @@ export default define(meta, paramDef, async (ps, me) => {
 	}
 
 	(async () => {
+		await removeLocalToRemoteFollowAll(user).catch(e => {});
+		await removeRemoteToLocalFollowAll(user).catch(e => {});
 		await doPostSuspend(user).catch(e => {});
 		await unFollowAll(user).catch(e => {});
 		await readAllNotify(user).catch(e => {});
@@ -58,6 +61,17 @@ export default define(meta, paramDef, async (ps, me) => {
 });
 
 async function unFollowAll(follower: User) {
+	// Follow Direction: Remote Susupended User to Local Follower
+
+	// When suspending a remote account, the account obviously doesn't
+	// actually become suspended on its origin server, i.e. unlike a
+	// locally suspended account it continues to have access to its home
+	// feed and other content. To prevent it from being able to continue
+	// to access toots it would receive because it follows local accounts,
+	// we have to force it to unfollow them. Unfortunately, there is no
+	// counterpart to this operation, i.e. you can't then force a remote
+	// account to re-follow you, so this part is not reversible.
+	if (follower.host == null) return;
 	const followings = await Followings.findBy({
 		followerId: follower.id,
 	});
@@ -69,6 +83,54 @@ async function unFollowAll(follower: User) {
 
 		if (followee == null) {
 			throw `Cant find followee ${following.followeeId}`;
+		}
+
+		await deleteFollowing(follower, followee, true);
+	}
+}
+
+async function removeLocalToRemoteFollowAll(follower: User) {
+	// Follow Direction: Local Susupended User to Remote Follower
+
+	// Local Suspended User is deleted on Remote, so Follow Relationships must be deleted.
+	// This part is not reversible.
+	if (follower.host != null) return;
+	const followings = await Followings.findBy({
+		followerId: follower.id,
+		followeeHost: Not(IsNull()),
+	});
+
+	for (const following of followings) {
+		const followee = await Users.findOneBy({
+			id: following.followeeId,
+		});
+
+		if (followee == null) {
+			throw `Cant find followee ${following.followeeId}`;
+		}
+
+		await deleteFollowing(follower, followee, true);
+	}
+}
+
+async function removeRemoteToLocalFollowAll(followee: User) {
+	// Follow Direction: Remote Follower to Local Susupended User
+
+	// Local Suspended User is deleted on Remote, so Follow Relationships must be deleted.
+	// This part is not reversible.
+	if (followee.host != null) return;
+	const followings = await Followings.findBy({
+		followeeId: followee.id,
+		followerHost: Not(IsNull()),
+	});
+
+	for (const following of followings) {
+		const follower = await Users.findOneBy({
+			id: following.followerId,
+		});
+
+		if (follower == null) {
+			throw `Cant find follower ${following.followerId}`;
 		}
 
 		await deleteFollowing(follower, followee, true);
