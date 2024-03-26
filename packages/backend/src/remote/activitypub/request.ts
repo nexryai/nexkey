e99d5f935ba152a4e0e1cbc447a717bd29cbe2eb
Laura Hausmann <laura@hausmann.dev>
2024-03-27 07:16:44 +0900

    fix (backend): verify object id host matches final URL when fetching remote activities
    https://iceshrimp.dev/iceshrimp/iceshrimp/commit/5f6096c1b7b37b771055e6e3b9d7b15ca10a05da
    
    Co-authored-by: naskya <m@naskya.net>

--- packages/backend/src/remote/activitypub/request.ts
+++ packages/backend/src/remote/activitypub/request.ts
@@ -1,95 +1,125 @@
-import config from "@/config/index.js";
-import { getUserKeypair } from "@/misc/keypair-store.js";
-import { User, ILocalUser } from "@/models/entities/user.js";
-import { getResponse } from "@/misc/fetch.js";
-import { createSignedPost, createSignedGet } from "./ap-request.js";
-import { IObject } from "./type.js";
-import type { Response } from "node-fetch";
+import config from '@/config/index.js';
+import { getUserKeypair } from '@/misc/keypair-store.js';
+import { User, ILocalUser } from '@/models/entities/user.js';
+import { StatusError, getResponse } from '@/misc/fetch.js';
+import { createSignedPost, createSignedGet } from './ap-request.js';
+import type { Response } from 'node-fetch';
+import { IObject } from './type.js';
+import { isValidUrl } from '@/misc/is-valid-url.js';
+import { apLogger } from '@/remote/activitypub/logger.js';
 
-export default async (user: { id: User["id"] }, url: string, object: any) => {
-    const body = JSON.stringify(object);
+export default async (user: { id: User['id'] }, url: string, object: any) => {
+	const body = JSON.stringify(object);
 
-    const keypair = await getUserKeypair(user.id);
+	const keypair = await getUserKeypair(user.id);
 
-    const req = createSignedPost({
-        key: {
-            privateKeyPem: keypair.privateKey,
-            keyId: `${config.url}/users/${user.id}#main-key`,
-        },
-        url,
-        body,
-        additionalHeaders: {
-            "User-Agent": config.userAgent,
-        },
-    });
+	const req = createSignedPost({
+		key: {
+			privateKeyPem: keypair.privateKey,
+			keyId: `${config.url}/users/${user.id}#main-key`,
+		},
+		url,
+		body,
+		additionalHeaders: {
+			'User-Agent': config.userAgent,
+		},
+	});
 
-    await getResponse({
-        url,
-        method: req.request.method,
-        headers: req.request.headers,
-        body,
-    });
+	await getResponse({
+		url,
+		method: req.request.method,
+		headers: req.request.headers,
+		body,
+	});
 };
 
 /**
- * Get AP object
- * @param user http-signature user
+ * Get ActivityPub object
  * @param url URL to fetch
+ * @param user http-signature user
+ * @param redirects whether or not to accept redirects
  */
-export async function apGet(url: string, user?: ILocalUser): Promise<IObject> {
-    let res: Response;
+export async function apGet(
+	url: string,
+	user?: ILocalUser,
+	redirects: boolean = true,
+): Promise<{ finalUrl: string; content: IObject }> {
+	if (!isValidUrl(url)) {
+		throw new StatusError('Invalid URL', 400);
+	}
 
-    if (user != null) {
-        const keypair = await getUserKeypair(user.id);
-        const req = createSignedGet({
-            key: {
-                privateKeyPem: keypair.privateKey,
-                keyId: `${config.url}/users/${user.id}#main-key`,
-            },
-            url,
-            additionalHeaders: {
-                "User-Agent": config.userAgent,
-            },
-        });
+	let res: Response;
+	
+	if (user != null) {
+		const keypair = await getUserKeypair(user.id);
+		const req = createSignedGet({
+			key: {
+				privateKeyPem: keypair.privateKey,
+				keyId: `${config.url}/users/${user.id}#main-key`,
+			},
+			url,
+			additionalHeaders: {
+				"User-Agent": config.userAgent,
+			},
+		});
 
-        res = await getResponse({
-            url,
-            method: req.request.method,
-            headers: req.request.headers,
-        });
-    } else {
-        res = await getResponse({
-            url,
-            method: "GET",
-            headers: {
-                Accept:
-                    "application/activity+json, application/ld+json; profile=\"https://www.w3.org/ns/activitystreams\"",
-                "User-Agent": config.userAgent,
-            },
-        });
-    }
+		res = await getResponse({
+			url,
+			method: req.request.method,
+			headers: req.request.headers,
+			redirect: redirects ? 'manual' : 'error',
+		});
 
-    const contentType = res.headers.get("content-type");
-    if (contentType == null || !validateContentType(contentType)) {
-        throw new Error("Invalid Content Type");
-    }
+		if (redirects && [301, 302, 307, 308].includes(res.status)) {
+			const newUrl = res.headers.get('location');
+			if (newUrl == null) throw new Error('apGet got redirect but no target location');
+			apLogger.debug(`apGet is redirecting to ${newUrl}`);
+			return apGet(newUrl, user, false);
+		}
+	} else {
+		res = await getResponse({
+			url,
+			method: "GET",
+			headers: {
+				Accept:
+					'application/activity+json, application/ld+json; profile="https://www.w3.org/ns/activitystreams"',
+				"User-Agent": config.userAgent,
+			},
+			redirect: redirects ? 'manual' : 'error',
+		});
 
-    if (res.body == null) throw new Error("body is null");
+		if (redirects && [301, 302, 307, 308].includes(res.status)) {
+			const newUrl = res.headers.get('location');
+			if (newUrl == null) throw new Error('apGet got redirect but no target location');
+			apLogger.debug(`apGet is redirecting to ${newUrl}`);
+			return apGet(newUrl, undefined, false);
+		}
+	}
 
-    const text = await res.text();
-    if (text.length > 65536) throw new Error("too big result");
+	const contentType = res.headers.get("content-type");
+	if (contentType == null || !validateContentType(contentType)) {
+		throw new Error(`apGet response had unexpected content-type: ${contentType}`);
+	}
 
-    return JSON.parse(text) as IObject;
+	if (res.body == null) throw new Error("body is null");
+
+	const text = await res.text();
+	if (text.length > 65536) throw new Error("too big result");
+
+	return {
+		finalUrl: res.url,
+		content: JSON.parse(text) as IObject,
+	};
 }
 
 function validateContentType(contentType: string): boolean {
-    const parts = contentType.split(/\s*;\s*/);
-    if (parts[0] === "application/activity+json") return true;
-    if (parts[0] !== "application/ld+json") return false;
-    return parts
-        .slice(1)
-        .some(
-            (part) =>
-                part.trim() === "profile=\"https://www.w3.org/ns/activitystreams\"",
-        );
+	const parts = contentType.split(/\s*;\s*/);
+	if (parts[0] === "application/activity+json") return true;
+	if (parts[0] !== "application/ld+json") return false;
+	return parts
+		.slice(1)
+		.some(
+			(part) =>
+				part.trim() === 'profile="https://www.w3.org/ns/activitystreams"',
+		);
 }
