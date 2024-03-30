b32dac67ca6596595dcf0b72c67f54718f210ead
naskya <m@naskya.net>
2024-03-30 18:54:25 +0900

    fix (backend): reject anonymous objects

--- packages/backend/src/remote/activitypub/resolver.ts
+++ packages/backend/src/remote/activitypub/resolver.ts
@@ -1,135 +1,172 @@
-import { ILocalUser } from "@/models/entities/user.js";
-import { getInstanceActor } from "@/services/instance-actor.js";
-import { fetchMeta } from "@/misc/fetch-meta.js";
-import { extractDbHost, isSelfHost } from "@/misc/convert-host.js";
-import { Notes, NoteReactions, Polls, Users } from "@/models/index.js";
-import renderNote from "@/remote/activitypub/renderer/note.js";
-import { renderLike } from "@/remote/activitypub/renderer/like.js";
-import { renderPerson } from "@/remote/activitypub/renderer/person.js";
-import renderQuestion from "@/remote/activitypub/renderer/question.js";
-import renderCreate from "@/remote/activitypub/renderer/create.js";
-import { renderActivity } from "@/remote/activitypub/renderer/index.js";
-import renderFollow from "@/remote/activitypub/renderer/follow.js";
-import { parseUri } from "./db-resolver.js";
-import { IObject, isCollectionOrOrderedCollection, ICollection, IOrderedCollection } from "./type.js";
-import { apGet } from "./request.js";
+import config from '@/config/index.js';
+import { ILocalUser } from '@/models/entities/user.js';
+import { getInstanceActor } from '@/services/instance-actor.js';
+import { fetchMeta } from '@/misc/fetch-meta.js';
+import { extractDbHost, isSelfHost } from '@/misc/convert-host.js';
+import { FollowRequests, Notes, NoteReactions, Polls, Users } from '@/models/index.js';
+import renderNote from '@/remote/activitypub/renderer/note.js';
+import { renderLike } from '@/remote/activitypub/renderer/like.js';
+import { renderPerson } from '@/remote/activitypub/renderer/person.js';
+import renderQuestion from '@/remote/activitypub/renderer/question.js';
+import renderCreate from '@/remote/activitypub/renderer/create.js';
+import { renderActivity } from '@/remote/activitypub/renderer/index.js';
+import renderFollow from '@/remote/activitypub/renderer/follow.js';
+import { parseUri } from './db-resolver.js';
+import { IObject, isCollectionOrOrderedCollection, ICollection, IOrderedCollection } from './type.js';
+import { apGet } from './request.js';
+import { IsNull, Not } from 'typeorm';
 
 export default class Resolver {
-    private history: Set<string>;
-    private user?: ILocalUser;
-    private recursionLimit?: number;
+	private history: Set<string>;
+	private user?: ILocalUser;
+	private recursionLimit?: number;
 
-    constructor(recursionLimit = 100) {
-        this.history = new Set();
-        this.recursionLimit = recursionLimit;
-    }
+	constructor(recursionLimit = 100) {
+		this.history = new Set();
+		this.recursionLimit = recursionLimit;
+	}
 
-    public getHistory(): string[] {
-        return Array.from(this.history);
-    }
+	public getHistory(): string[] {
+		return Array.from(this.history);
+	}
 
-    public async resolveCollection(value: string | IObject): Promise<ICollection | IOrderedCollection> {
-        const collection = typeof value === "string"
-            ? await this.resolve(value)
-            : value;
+	public async resolveCollection(value: string | IObject): Promise<ICollection | IOrderedCollection> {
+		const collection = typeof value === 'string'
+			? await this.resolve(value)
+			: value;
 
-        if (isCollectionOrOrderedCollection(collection)) {
-            return collection;
-        } else {
-            throw new Error(`unrecognized collection type: ${collection.type}`);
-        }
-    }
+		if (isCollectionOrOrderedCollection(collection)) {
+			return collection;
+		} else {
+			throw new Error(`unrecognized collection type: ${collection.type}`);
+		}
+	}
 
-    public async resolve(value: string | IObject): Promise<IObject> {
-        if (value == null) {
-            throw new Error("resolvee is null (or undefined)");
-        }
+	public async resolve(value: string | IObject): Promise<IObject> {
+		if (value == null) {
+			throw new Error('resolvee is null (or undefined)');
+		}
 
-        if (typeof value !== "string") {
-            return value;
-        }
+		if (typeof value !== 'string') {
+			return value;
+		}
 
-        if (value.includes("#")) {
-            // URLs with fragment parts cannot be resolved correctly because
-            // the fragment part does not get transmitted over HTTP(S).
-            // Avoid strange behaviour by not trying to resolve these at all.
-            throw new Error(`cannot resolve URL with fragment: ${value}`);
-        }
+		if (value.includes('#')) {
+			// URLs with fragment parts cannot be resolved correctly because
+			// the fragment part does not get transmitted over HTTP(S).
+			// Avoid strange behaviour by not trying to resolve these at all.
+			throw new Error(`cannot resolve URL with fragment: ${value}`);
+		}
 
-        if (this.history.has(value)) {
-            throw new Error("cannot resolve already resolved one");
-        }
+		if (this.history.has(value)) {
+			throw new Error('cannot resolve already resolved one');
+		}
 
-        if (this.recursionLimit && this.history.size > this.recursionLimit) {
-            throw new Error("hit recursion limit");
-        }
+		if (this.recursionLimit && this.history.size > this.recursionLimit) {
+			throw new Error('hit recursion limit');
+		}
 
-        this.history.add(value);
+		this.history.add(value);
 
-        const host = extractDbHost(value);
-        if (isSelfHost(host)) {
-            return await this.resolveLocal(value);
-        }
+		const host = extractDbHost(value);
+		if (isSelfHost(host)) {
+			return await this.resolveLocal(value);
+		}
 
-        const meta = await fetchMeta();
-        if (meta.blockedHosts.some(x => host.endsWith(x))) {
-            throw new Error("Instance is blocked");
-        }
+		const meta = await fetchMeta();
+		if (meta.blockedHosts.some(x => host.endsWith(x))) {
+			throw new Error('Instance is blocked');
+		}
 
-        if (!this.user) {
-            this.user = await getInstanceActor();
-        }
+		if (config.signToActivityPubGet && !this.user) {
+			this.user = await getInstanceActor();
+		}
 
-        const object = await apGet(value, this.user);
+		const { finalUrl, content: object } = await apGet(value, this.user);
 
-        if (object == null || (
-            Array.isArray(object["@context"]) ?
-                !(object["@context"] as unknown[]).includes("https://www.w3.org/ns/activitystreams") :
-                object["@context"] !== "https://www.w3.org/ns/activitystreams"
-        )) {
-            throw new Error("invalid response");
-        }
+		if (object == null || (
+			Array.isArray(object['@context']) ?
+				!(object['@context'] as unknown[]).includes('https://www.w3.org/ns/activitystreams') :
+				object['@context'] !== 'https://www.w3.org/ns/activitystreams'
+		)) {
+			throw new Error('invalid response');
+		}
 
-        return object;
-    }
+		if (object.id == null) {
+			throw new Error('Object has no ID');
+		}
 
-    private resolveLocal(url: string): Promise<IObject> {
-        const parsed = parseUri(url);
-        if (!parsed.local) throw new Error("resolveLocal: not local");
+		if (finalUrl === object.id) return object;
 
-        switch (parsed.type) {
-            case "notes":
-                return Notes.findOneByOrFail({ id: parsed.id })
-				.then(note => {
-				    if (parsed.rest === "activity") {
-				        // this refers to the create activity and not the note itself
-				        return renderActivity(renderCreate(renderNote(note)));
-				    } else {
-				        return renderNote(note);
-				    }
+		if (new URL(finalUrl).host !== new URL(object.id).host) {
+			throw new Error("Object ID host doesn't match final url host");
+		}
+
+		const finalRes = await apGet(object.id, this.user);
+
+		if (finalRes.finalUrl !== finalRes.content.id)
+			throw new Error(
+				"Object ID still doesn't match final URL after second fetch attempt",
+			);
+
+		return finalRes.content;
+	}
+
+	private async resolveLocal(url: string): Promise<IObject> {
+		const parsed = parseUri(url);
+		if (!parsed.local) throw new Error('resolveLocal: not local');
+
+		switch (parsed.type) {
+			case 'notes':
+				const note = await Notes.findOneByOrFail({ id: parsed.id });
+				if (parsed.rest === 'activity') {
+					// this refers to the create activity and not the note itself
+					return renderActivity(renderCreate(renderNote(note), note));
+				} else {
+					return renderNote(note);
+				}
+			case 'users':
+				const user = await Users.findOneByOrFail({ id: parsed.id });
+				return await renderPerson(user as ILocalUser);
+			case 'questions':
+				// Polls are indexed by the note they are attached to.
+				const [pollNote, poll] = await Promise.all([
+					Notes.findOneByOrFail({ id: parsed.id }),
+					Polls.findOneByOrFail({ noteId: parsed.id }),
+				]);
+				return await renderQuestion({ id: pollNote.userId }, pollNote, poll);
+			case 'likes':
+				const reaction = await NoteReactions.findOneByOrFail({ id: parsed.id });
+				return renderActivity(renderLike(reaction, { uri: null }));
+			case 'follows':
+				// if rest is a <followee id>
+				if (parsed.rest != null && /^\w+$/.test(parsed.rest)) {
+					const [follower, followee] = await Promise.all(
+						[parsed.id, parsed.rest].map((id) => Users.findOneByOrFail({ id })));
+					return renderActivity(renderFollow(follower, followee, url));
+				}
+
+				// Another situation is there is only requestId, then obtained object from database.
+				const followRequest = await FollowRequests.findOneBy({
+					id: parsed.id,
 				});
-            case "users":
-                return Users.findOneByOrFail({ id: parsed.id })
-				.then(user => renderPerson(user as ILocalUser));
-            case "questions":
-                // Polls are indexed by the note they are attached to.
-                return Promise.all([
-                    Notes.findOneByOrFail({ id: parsed.id }),
-                    Polls.findOneByOrFail({ noteId: parsed.id }),
-                ])
-				.then(([note, poll]) => renderQuestion({ id: note.userId }, note, poll));
-            case "likes":
-                return NoteReactions.findOneByOrFail({ id: parsed.id }).then(reaction => renderActivity(renderLike(reaction, { uri: null })));
-            case "follows":
-                // rest should be <followee id>
-                if (parsed.rest == null || !/^\w+$/.test(parsed.rest)) throw new Error("resolveLocal: invalid follow URI");
-
-                return Promise.all(
-                    [parsed.id, parsed.rest].map(id => Users.findOneByOrFail({ id })),
-                )
-				.then(([follower, followee]) => renderActivity(renderFollow(follower, followee, url)));
-            default:
-                throw new Error(`resolveLocal: type ${type} unhandled`);
-        }
-    }
+				if (followRequest == null) {
+					throw new Error('resolveLocal: invalid follow URI');
+				}
+				const follower = await Users.findOneBy({
+					id: followRequest.followerId,
+					host: IsNull(),
+				});
+				const followee = await Users.findOneBy({
+					id: followRequest.followeeId,
+					host: Not(IsNull()),
+				});
+				if (follower == null || followee == null) {
+					throw new Error('resolveLocal: invalid follow URI');
+				}
+				return renderActivity(renderFollow(follower, followee, url));
+			default:
+				throw new Error(`resolveLocal: type ${parsed.type} unhandled`);
+		}
+	}
 }
